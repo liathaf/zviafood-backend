678c093767001c19a5b9acf24a3e5f99f4f96d57
Liat <liathaf@gmail.com>
2021-03-01 17:54:40 +0200

    initial commit

---
+++ config/index.js
@@ -0,0 +1,14 @@
+var config;
+
+if (process.env.NODE_ENV === 'production') {
+
+    config = require('./prod');
+
+} else {
+    config =  require('./dev')
+}
+
+module.exports = config;
+
+
+
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {    
+    "dbURL": "mongodb+srv://liat:unitmo12@cluster0.lx82h.mongodb.net/recipe_db?retryWrites=true", 
+  }
+  
